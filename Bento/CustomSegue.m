a5d77c58980af0d179d624467594f0992ddfec66
Joseph Lau <forwardintent@gmail.com>
2015-04-30 20:54:52 -0700

    reverted segue back to push

---
+++ Bento/CustomSegue.m
@@ -0,0 +1,31 @@
+//
+//  CustomSegue.m
+//  VineBoard
+//
+//  Created by Joseph Lau on 10/31/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+#import "CustomSegue.h"
+#import "QuartzCore/QuartzCore.h"
+
+@implementation CustomSegue
+
+-(void)perform {
+    
+    UIViewController *sourceViewController = (UIViewController*)[self sourceViewController];
+    UIViewController *destinationController = (UIViewController*)[self destinationViewController];
+    
+    CATransition* transition = [CATransition animation];
+    transition.duration = .25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionFade; //kCATransitionMoveIn; //, kCATransitionPush, kCATransitionReveal, kCATransitionFade
+    transition.subtype = kCATransitionFromLeft; //kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop, kCATransitionFromBottom
+    
+    [sourceViewController.navigationController.view.layer addAnimation:transition
+                                                                forKey:kCATransition];
+    
+    [sourceViewController.navigationController pushViewController:destinationController animated:NO];
+}
+
+@end
