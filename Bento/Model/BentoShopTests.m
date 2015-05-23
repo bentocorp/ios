ad33eacf670fa4046d625675f33348a4f8ace976
Joseph Lau <josephlausf@gmail.com>
2015-05-22 17:43:38 -0700

    added BentoShopTests

---
+++ Bento/Model/BentoShopTests.m
@@ -0,0 +1,28 @@
+//
+//  BentoShopTests.m
+//  Bento
+//
+//  Created by Joseph Lau on 5/22/15.
+//  Copyright (c) 2015 bentonow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BentoShopTests : XCTestCase
+
+@end
+
+@implementation BentoShopTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+
+
+@end
