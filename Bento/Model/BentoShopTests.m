ab9ed55dacb0c7930991618d23b65d979e30a145
Joseph Lau <josephlausf@gmail.com>
2015-06-03 14:24:38 -0700

    isSoldOut testing

--- Bento/Model/BentoShopTests.m
+++ Bento/Model/BentoShopTests.m
@@ -15,9 +15,79 @@
 @end
 
 @implementation BentoShopTests
+{
+    NSDictionary *dicStatus1;
+    NSDictionary *dicStatus2;
+}
 
 - (void)setUp {
     [super setUp];
+    
+    // mock CLOSED
+    dicStatus1 = @{
+                    @"menu": @[
+                                @{
+                                    @"itemId": @"25",
+                                    @"qty": @"9"
+                                },
+                                @{
+                                    @"itemId": @"35",
+                                    @"qty": @"9"
+                                },
+                                @{
+                                    @"itemId": @"39",
+                                    @"qty": @"9"
+                                },
+                                @{
+                                    @"itemId": @"42",
+                                    @"qty": @"9"
+                                },
+                                @{
+                                    @"itemId": @"51",
+                                    @"qty": @"9"
+                                },
+                                @{
+                                    @"itemId": @"52",
+                                    @"qty": @"9"
+                                }
+                             ],
+                    @"overall": @{
+                                    @"value": @"closed"
+                                 }
+                    };
+    
+    // mock SOLD OUT
+    dicStatus2 = @{
+                   @"menu": @[
+                               @{
+                                   @"itemId": @"25",
+                                   @"qty": @"9"
+                                },
+                               @{
+                                   @"itemId": @"35",
+                                   @"qty": @"9"
+                                },
+                               @{
+                                   @"itemId": @"39",
+                                   @"qty": @"9"
+                                },
+                               @{
+                                   @"itemId": @"42",
+                                   @"qty": @"9"
+                                },
+                               @{
+                                   @"itemId": @"51",
+                                   @"qty": @"9"
+                                },
+                               @{
+                                   @"itemId": @"52",
+                                   @"qty": @"9"
+                                }
+                             ],
+                    @"overall": @{
+                                   @"value": @"sold out"
+                                 }
+                   };
 }
 
 - (void)tearDown {
@@ -28,7 +98,9 @@
 {
     BOOL isClosed;
     
-    NSString *strOverallStatus = @"closed"; // mock
+    /*----------------------------------------*/
+    
+    NSString *strOverallStatus = dicStatus1[@"overall"][@"value"];
     
     if ([strOverallStatus isEqualToString:@"closed"])
         isClosed = YES;
@@ -42,7 +114,9 @@
 {
     BOOL isClosed;
     
-    NSString *strOverallStatus = @"open"; // mock
+    /*----------------------------------------*/
+    
+    NSString *strOverallStatus = dicStatus2[@"overall"][@"value"];
     
     if ([strOverallStatus isEqualToString:@"closed"])
         isClosed = YES;
@@ -52,16 +126,40 @@
     XCTAssert(isClosed == NO, @"It's open!");
 }
 
-- (void)testIsSoldOut
+- (void)testIsSoldOutYES
 {
-    if (self.dicStatus == nil)
-        return YES;
+    BOOL isSoldOut;
     
-    NSString *strOverallStatus = self.dicStatus[@"overall"][@"value"];
+    /*----------------------------------------*/
+    
+    if (dicStatus2 == nil)
+        isSoldOut = YES;
+    
+    NSString *strOverallStatus = dicStatus2[@"overall"][@"value"];
     if ([strOverallStatus isEqualToString:@"sold out"])
-        return YES;
+        isSoldOut = YES;
+    else
+        isSoldOut = NO;
     
-    return NO;
+    XCTAssert(isSoldOut == YES, @"It's not sold out!");
+}
+
+- (void)testIsSoldOutNO
+{
+    BOOL isSoldOut;
+    
+    /*----------------------------------------*/
+    
+    if (dicStatus1 == nil)
+        isSoldOut = YES;
+    
+    NSString *strOverallStatus = dicStatus1[@"overall"][@"value"];
+    if ([strOverallStatus isEqualToString:@"sold out"])
+        isSoldOut = YES;
+    else
+        isSoldOut = NO;
+    
+    XCTAssert(isSoldOut == NO, @"It's sold out!");
 }
 
 @end
