7ecc1812b4dfd3c95c14f5029d3df84a163c6509
Joseph Lau <josephlausf@gmail.com>
2015-08-04 17:23:30 -0700

    check for nil before inserting to mixpanel dictionary object

--- Bento/ViewController/SignIn/RegisterViewController.m
+++ Bento/ViewController/SignIn/RegisterViewController.m
@@ -499,11 +499,32 @@
         // identify user for current session
         [mixpanel identify:strEmail];
         
+        NSString *currentDate = [self getCurrentDate];
+        
+        NSString *sourceFinal;
+        NSString *currentDateFinal;
+        NSString *currentAddressFinal;
+        
+        if (source != nil)
+            sourceFinal = source;
+        else
+            sourceFinal = @"N/A ";
+        
+        if (currentDate != nil)
+            currentDateFinal = currentDate;
+        else
+            currentDateFinal = @"N/A";
+        
+        if (currentAddress != nil)
+            currentAddressFinal = currentAddress;
+        else
+            currentAddressFinal = @"N/A";
+        
         // set initial properties once
         [mixpanel.people setOnce:@{
-                                   @"Installed Source":source,
-                                   @"$created": [self getCurrentDate],
-                                   @"Sign Up Address": currentAddress
+                                   @"Installed Source":sourceFinal,
+                                   @"$created": currentDateFinal,
+                                   @"Sign Up Address": currentAddressFinal
                                    }];
         
         // set properties
@@ -512,8 +533,6 @@
                                @"$email": strEmail,
                                @"$phone": strPhoneNumber,
                                }];
-        
-        NSLog(@"%@, %@, %@, %@, %@, %@, %@", mixpanel.distinctId, source, [self getCurrentDate], currentAddress, strUserName, strEmail, strPhoneNumber);
         
 /*--------------------------------------------------------------------*/
         
