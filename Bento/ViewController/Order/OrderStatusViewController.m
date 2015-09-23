2fe7141d6acce8b0828d8c51cef04c665a156295
Joseph Lau <josephlausf@gmail.com>
2015-09-23 15:06:42 -0700

    added navigation bar, annotation marker

--- Bento/ViewController/Order/OrderStatusViewController.m
+++ Bento/ViewController/Order/OrderStatusViewController.m
@@ -6,7 +6,11 @@
 //  Copyright © 2015 bentonow. All rights reserved.
 //
 
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
 #import "OrderStatusViewController.h"
+#import "UIColor+CustomColors.h"
 #import "Mapbox.h"
 
 @interface OrderStatusViewController () <MGLMapViewDelegate>
@@ -20,8 +24,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // navigation bar color
+    UIView *navigationBarView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 65)];
+    navigationBarView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:navigationBarView];
+    
+    // title label
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH / 2 - 110, 20, 220, 45)];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.font = [UIFont fontWithName:@"OpenSans-Bold" size:16.0f];
+    titleLabel.textColor = [UIColor bentoTitleGray];
+    titleLabel.text = @"Order Status";
+    [self.view addSubview:titleLabel];
+    
+    // back button
+    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 20, 50, 45)];
+    [closeButton setImage:[UIImage imageNamed:@"nav_btn_close"] forState:UIControlStateNormal];
+    [closeButton addTarget:self action:@selector(onCloseButton) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:closeButton];
+    
+    // line separator under nav bar
+    UIView *longLineSepartor1 = [[UIView alloc] initWithFrame:CGRectMake(0, 65, SCREEN_WIDTH, 1)];
+    longLineSepartor1.backgroundColor = [UIColor colorWithRed:0.827f green:0.835f blue:0.835f alpha:1.0f];
+    [self.view addSubview:longLineSepartor1];
+    
     // initialize the map view
-    self.mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
+    self.mapView = [[MGLMapView alloc] initWithFrame:CGRectMake(0, 66, SCREEN_WIDTH, SCREEN_HEIGHT-66)];
     self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     
     // set the map's center coordinate
@@ -45,6 +73,11 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (void)onCloseButton
+{
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
