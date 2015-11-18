17e609dce74e4eb94814664d208b467636d9ebe4
Joseph Lau <josephlausf@gmail.com>
2015-11-17 16:57:34 -0800

    added addons tvc and vc

---
+++ Bento/ViewController/Order/AddonsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AddonsTableViewCell.h
+//  Bento
+//
+//  Created by Joseph Lau on 11/17/15.
+//  Copyright © 2015 bentonow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddonsTableViewCell : UITableViewCell
+
+@end
---
+++ Bento/ViewController/Order/AddonsTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  AddonsTableViewCell.m
+//  Bento
+//
+//  Created by Joseph Lau on 11/17/15.
+//  Copyright © 2015 bentonow. All rights reserved.
+//
+
+#import "AddonsTableViewCell.h"
+
+@implementation AddonsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
