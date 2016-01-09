b631c68b7e5c7499f21773ecc89582a83b7c1922
Joseph Lau <josephlausf@gmail.com>
2016-01-08 16:47:48 -0800

    increased scrollview width

--- Bento/ViewController/MakeBento/HomeViewController.m
+++ Bento/ViewController/MakeBento/HomeViewController.m
@@ -38,7 +38,7 @@
     [self.scrollView addSubview:fixedVC.view];
     [fixedVC didMoveToParentViewController:self];
     
-    self.scrollView.contentSize = CGSizeMake(640, SCREEN_HEIGHT);
+    self.scrollView.contentSize = CGSizeMake(SCREEN_WIDTH * 2, SCREEN_HEIGHT);
     self.scrollView.pagingEnabled = NO;
     self.scrollView.bounces = NO;
 }
@@ -47,15 +47,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
