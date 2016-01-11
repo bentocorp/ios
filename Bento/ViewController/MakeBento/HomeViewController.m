2452657e8e4920d07cacee64d946db4096546179
Joseph Lau <josephlausf@gmail.com>
2016-01-11 10:49:48 -0800

    removed fixedvc from home vc

--- Bento/ViewController/MakeBento/HomeViewController.m
+++ Bento/ViewController/MakeBento/HomeViewController.m
@@ -11,8 +11,8 @@
 
 #import "HomeViewController.h"
 #import "CustomViewController.h"
-#import "FixedViewController.h"
 #import "PreviewViewController.h"
+
 #import "BentoShop.h"
 
 @interface HomeViewController ()
@@ -20,7 +20,7 @@
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @property (nonatomic) CustomViewController *customVC;
-@property (nonatomic) FixedViewController *fixedVC;
+@property (nonatomic) PreviewViewController *previewVC;
 
 @end
 
@@ -29,38 +29,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSString *menuType = [[BentoShop sharedInstance] getMenuType];
+    // Custom View
+    self.customVC = [[CustomViewController alloc] init];
+    [self addChildViewController:self.customVC];
+    [self.scrollView addSubview:self.customVC.view];
+    [self.customVC didMoveToParentViewController:self];
     
-    if ([menuType isEqualToString:@"custom"]) {
-        self.customVC = [[CustomViewController alloc] init];
-        [self addChildViewController:self.customVC];
-        [self.scrollView addSubview:self.customVC.view];
-        [self.customVC didMoveToParentViewController:self];
-    }
-    else {
-        self.fixedVC = [[FixedViewController alloc] init];
-        [self addChildViewController:self.fixedVC];
-        [self.scrollView addSubview:self.fixedVC.view];
-        [self.fixedVC didMoveToParentViewController:self];
-    }
-    
-    PreviewViewController *previewVC = [[PreviewViewController alloc] init];
-    CGRect frame = previewVC.view.frame;
+    // Preview View
+    self.previewVC = [[PreviewViewController alloc] init];
+    CGRect frame = self.previewVC.view.frame;
     frame.origin.x = SCREEN_WIDTH;
-    previewVC.view.frame = frame;
+    self.previewVC.view.frame = frame;
     
-    [self addChildViewController:previewVC];
-    [self.scrollView addSubview:previewVC.view];
-    [previewVC didMoveToParentViewController:self];
+    [self addChildViewController:self.previewVC];
+    [self.scrollView addSubview:self.previewVC.view];
+    [self.previewVC didMoveToParentViewController:self];
     
+    // Scroll View
     self.scrollView.contentSize = CGSizeMake(SCREEN_WIDTH * 2, SCREEN_HEIGHT);
     self.scrollView.pagingEnabled = NO;
     self.scrollView.bounces = NO;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
