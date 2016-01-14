769b851075909eb2b9abc24ce5d254fee583ddcb
Joseph Lau <josephlausf@gmail.com>
2016-01-13 17:38:06 -0800

    added 5podcustomVC

---
+++ Bento/ViewController/MakeBento/5PodCustomViewController.h
@@ -0,0 +1,13 @@
+//
+//  5PodCustomViewController.h
+//  Bento
+//
+//  Created by Joseph Lau on 1/13/16.
+//  Copyright © 2016 bentonow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _PodCustomViewController : UIViewController
+
+@end
---
+++ Bento/ViewController/MakeBento/5PodCustomViewController.m
@@ -0,0 +1,37 @@
+//
+//  5PodCustomViewController.m
+//  Bento
+//
+//  Created by Joseph Lau on 1/13/16.
+//  Copyright © 2016 bentonow. All rights reserved.
+//
+
+#import "5PodCustomViewController.h"
+
+@interface _PodCustomViewController ()
+
+@end
+
+@implementation _PodCustomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
