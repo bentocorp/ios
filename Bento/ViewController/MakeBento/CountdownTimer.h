56e8283b0b83172cfa16a3774ccae0c98d5050e9
Joseph Lau <josephlausf@gmail.com>
2016-01-21 13:05:21 -0800

    added CountdownTimer class

---
+++ Bento/ViewController/MakeBento/CountdownTimer.h
@@ -0,0 +1,13 @@
+//
+//  CountdownTimer.h
+//  Bento
+//
+//  Created by Joseph Lau on 1/21/16.
+//  Copyright © 2016 bentonow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CountdownTimer : NSObject
+
+@end
---
+++ Bento/ViewController/MakeBento/CountdownTimer.m
@@ -0,0 +1,13 @@
+//
+//  CountdownTimer.m
+//  Bento
+//
+//  Created by Joseph Lau on 1/21/16.
+//  Copyright © 2016 bentonow. All rights reserved.
+//
+
+#import "CountdownTimer.h"
+
+@implementation CountdownTimer
+
+@end
