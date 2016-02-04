34f923f2b953f6b4cd2d2bfef0660aa81fed56e1
Joseph Lau <josephlausf@gmail.com>
2016-02-03 16:14:54 -0800

    reduced height of address view

---
+++ Bento/ViewController/Order/SummaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  SummaryViewController.h
+//  Bento
+//
+//  Created by Joseph Lau on 2/3/16.
+//  Copyright © 2016 bentonow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummaryViewController : UIViewController
+
+@end
