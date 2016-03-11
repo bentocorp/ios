e15b4b01985a92c92e405e13e6617fa076c288ca
Joseph Lau <josephlausf@gmail.com>
2016-03-11 14:13:20 -0800

    added daily notifications cell

---
+++ Bento/Cell/DailyNotificationsCell.h
@@ -0,0 +1,17 @@
+//
+//  DailyNotificationsCell.h
+//  Bento
+//
+//  Created by Joseph Lau on 3/11/16.
+//  Copyright © 2016 bentonow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailyNotificationsCell : UITableViewCell
+
+@property (nonatomic) UIImageView *iconImageView;
+@property (nonatomic) UILabel *settingsLabel;
+@property (nonatomic) UISwitch *toggle;
+
+@end
---
+++ Bento/Cell/DailyNotificationsCell.m
@@ -0,0 +1,23 @@
+//
+//  DailyNotificationsCell.m
+//  Bento
+//
+//  Created by Joseph Lau on 3/11/16.
+//  Copyright © 2016 bentonow. All rights reserved.
+//
+
+#import "DailyNotificationsCell.h"
+
+@implementation DailyNotificationsCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
